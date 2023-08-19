2de75e07b0e82d4b667133337eefed671ffa2d6f
Yousef6677oo <yousefahmedkamel7766@gmail.com>
2023-08-20 01:10:59 +0300

    +sign_in_screen

---
+++ lib/ui/sign_in/sign_in_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  static const String routeName = "sign_in_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
